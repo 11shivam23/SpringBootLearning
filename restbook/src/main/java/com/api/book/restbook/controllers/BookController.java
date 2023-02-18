34229b5048f17a70d2447256d204d9546cb1c88f
11shivam23 <11shivam23@gmail.com>
2023-02-18 20:00:00 +0530

    rest api

---
+++ restbook/src/main/java/com/api/book/restbook/controllers/BookController.java
@@ -0,0 +1,50 @@
+package com.api.book.restbook.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.book.restbook.entities.Book;
+import com.api.book.restbook.services.BookService;
+
+// @Controller  
+@RestController  //if we are using restcontroller then there is no need of @ResponseBody Annootation
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    // @RequestMapping(value = "/books", method = RequestMethod.GET)
+    // @ResponseBody    
+    @GetMapping("/books")
+    public String getBooks() {
+        return "this is testing book first";
+    }
+
+    @GetMapping("/bookObject")
+    public Book getBookObject() {
+
+        Book book = new Book();
+        book.setId(0);
+        book.setTitle("Java Complete Reference");
+        book.setAuthor("Shivam");
+        return book;
+    }
+
+    @GetMapping("/allBooks")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/singleBook/{id}")
+    public Book getBook(@PathVariable("id") int id) {
+        return bookService.getBookById(id);
+    }
+}
---
+++ restbook/src/main/java/com/api/book/restbook/services/BookService.java
@@ -0,0 +1,32 @@
+package com.api.book.restbook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.api.book.restbook.entities.Book;
+
+@Service
+public class BookService {
+    
+    private static List<Book> list = new ArrayList<>();
+
+    static {
+        list.add(new Book(1, "Rich Dad", "piyush"));
+        list.add(new Book(2, "Poor Dad", "Akashay"));
+        list.add(new Book(3, "Avg Dad", "Pranav"));
+    }
+
+    //getAllBooks
+    public List<Book> getAllBooks() {
+        return list;
+    }
+
+    //getSingleBook
+    public Book getBookById(int id) {
+        Book book = null;
+        book = list.stream().filter(e->e.getId()==id).findFirst().get();
+        return book;
+    }
+}
