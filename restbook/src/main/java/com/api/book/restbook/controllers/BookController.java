fe5c0ab5508a7026439bcaef66e9b639ec38db33
11shivam23 <11shivam23@gmail.com>
2023-02-19 01:10:21 +0530

    Post mapping

--- restbook/src/main/java/com/api/book/restbook/controllers/BookController.java
+++ restbook/src/main/java/com/api/book/restbook/controllers/BookController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.book.restbook.entities.Book;
@@ -47,4 +49,9 @@ public class BookController {
     public Book getBook(@PathVariable("id") int id) {
         return bookService.getBookById(id);
     }
+
+   @PostMapping("/addBook")
+   public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+   }
 }
