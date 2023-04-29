60e2927632008b86660e3191a9393d696a205fc6
11shivam23 <11shivam23@gmail.com>
2023-04-30 01:45:55 +0530

    form validation

---
+++ formvalidation/src/main/java/com/example/formvalidation/formvalidation/FormvalidationApplication.java
@@ -0,0 +1,14 @@
+package com.example.formvalidation.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormvalidationApplication.class, args);
+	}
+
+}
+
